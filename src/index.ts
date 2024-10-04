eec824ffb0924f9281c605bac4d4cc4271c00df6
Ibarra <ibarra2002x3@gmail.com>
2024-10-03 18:44:14 -0600

    refactor: Update styles and add login functionality

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,9 @@
 import "dotenv/config";
 import app from "./app";
-import { GameModel } from "./models/GameModel";
+import { AuthModel } from "./models/AuthModel";
 const PORT = 3000;
 
 app.listen(PORT, async () => {
-  await GameModel.sync({ force: true });
-  const games = [
-    {
-      id: 1,
-      title: "Stardew Valley",
-      description: "Juego de Granja",
-      img: "stardew.png",
-      downloadLink: "https://www.google.com",
-      tags: ["Granja", "Simulacion", "Multijugador"],
-    },
-    {
-      id: 2,
-      title: "Zelda",
-      description: "Juego de aventuras",
-      img: "zelda.png",
-      downloadLink: "https://www.google.com",
-      tags: ["Aventura", "RPG"],
-    },
-    {
-      id: 3,
-      title: "Metroid",
-      description: "Juego de acción",
-      img: "metroid.png",
-      downloadLink: "https://www.google.com",
-      tags: ["Aventura", "Shooter"],
-    },
-    {
-      id: 4,
-      title: "Castlevania",
-      description: "Juego de plataformas",
-      img: "castlevania.png",
-      downloadLink: "https://www.google.com",
-      tags: ["Plataformas", "Aventura"],
-    },
-    {
-      id: 5,
-      title: "Final Fantasy",
-      description: "Juego de rol",
-      img: "final-fantasy.png",
-      downloadLink: "https://www.google.com",
-      tags: ["RPG", "Aventura"],
-    },
-  ];
-
-  await GameModel.bulkCreate(games);
-
+  await AuthModel.sync();
   console.log(`Server listening on http://localhost:${PORT}`);
 });
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { showLogin } from "../controllers/AuthController";
+import { AdminOnly } from "../middlewares/AuthMiddleware";
+import { login, showDashboard, showLogin } from "../controllers/AuthController";
 const router = Router();
 
 router.get("/login", showLogin);
-
+router.post("/login", login);
+router.get("/dashboard", [AdminOnly], showDashboard);
 export { router as AuthRouter };
