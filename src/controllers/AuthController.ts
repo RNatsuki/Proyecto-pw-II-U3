474cd456d1dc61087730c21110436020ea3a31cb
Ibarra <ibarra2002x3@gmail.com>
2024-10-03 20:13:06 -0600

    Refactor styles and add login functionality

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { GameModel } from "../models/GameModel";
 import { AuthModel } from "../models/AuthModel";
 export async function showLogin(req: Request, res: Response) {
   res.render("login");
@@ -30,5 +31,10 @@ export async function logout(req: Request, res: Response) {
 }
 
 export async function showDashboard(req: Request, res: Response) {
-  res.render("dashboard", { user: req.session.user });
+  const games = await GameModel.findAll();
+
+  const gamesJSON = games.map((game) => game.toJSON());
+  console.log(gamesJSON);
+
+  res.render("dashboard", { user: req.session.user, games: gamesJSON });
 }
